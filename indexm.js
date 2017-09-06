add2ac81e2e741e101ae5931dacd1895ac97d180
hobagolyka <toth.jdt@gmail.com>
2017-09-06 19:24:19 +0200

    sdfg

--- indexm.js
+++ indexm.js
@@ -4,9 +4,9 @@ var path = require('path');
 
 app.use(express.static('public'));
 
-// viewed at http://localhost:8080
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/view/index.html'));
 });
-//dfgsdfg
-app.listen(3004);
+
+app.listen(3001);
